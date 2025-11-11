3c96ce96f2d6781bba0646f4e6040a9b50d611e5
star-tide <bnesslage223@yahoo.com>
2025-11-11 02:47:47 -0500

    Added a main menu. Functioning Exit, as well as starting the game. Big first steps!

---
+++ scripts/ui/main_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var anim_player = $Title/AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim_player.play("float")
+
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+
+
+func _on_host_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/3d/Main3D.tscn")
+
+
+func _on_join_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
